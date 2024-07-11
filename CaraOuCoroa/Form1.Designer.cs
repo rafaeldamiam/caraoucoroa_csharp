093dfbdd15cc043bae4f08fb96caa323053739ac
rafaeldamiam <rafael.damiam23@gmail.com>
2024-07-11 12:51:22 -0300

    First Commit

---
+++ CaraOuCoroa/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CaraOuCoroa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(96, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Jogar Moeda";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton1.Location = new System.Drawing.Point(70, 112);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 24);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Cara";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton2.Location = new System.Drawing.Point(200, 112);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 24);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Coroa";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(64, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cara ou Coroa?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 260);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Cara ou Coroa?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CaraOuCoroa/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaraOuCoroa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CRIA VALOR ALEATORIO
+            Random rnd = new Random();
+            int nums = rnd.Next(0, 100);
+            int result = 0;
+            int rb = 0;
+
+
+            if (nums <= 50)
+            {
+                //MessageBox.Show("O resultado é cara!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                result = 1;
+            }
+            else if (nums >= 50)
+            {
+                //MessageBox.Show("O resultado é coroa!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                result = 2;
+            }
+
+            //PEGA VALOR DO CHECKBOX
+            if (radioButton1.Checked)
+            {
+                rb = 1;
+                MessageBox.Show("Você selecionou cara!");
+            }
+            else if (radioButton2.Checked)
+            {
+                rb = 2;
+                MessageBox.Show("Você selecionou coroa!");
+            }
+            else
+            {
+                rb = 0;
+                MessageBox.Show("Não houve nenhuma seleção!");
+            }
+
+            if ((result == 1))
+            {
+                if ((rb == result))
+                {
+                    //resultado está certo e é cara
+                    MessageBox.Show("O resultado é cara!", "Acertou!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    //resultado está errado e é cara
+                    MessageBox.Show("O resultado é cara!", "Errou!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            if ((result == 2))
+            {
+                if ((rb == result))
+                {
+                    //resultado está certo e é coroa
+                    MessageBox.Show("O resultado é coroa!", "Acertou!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    //resultado está errado e é coroa
+                    MessageBox.Show("O resultado é coroa!", "Errou!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+        }
+    }
+}
